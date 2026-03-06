44ac771d9742dd800c52a17a944694a6efde97b2
Joseph-maloney <joey.c.maloney@gmail.com>
2026-03-06 11:54:18 -0800

    local host

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
